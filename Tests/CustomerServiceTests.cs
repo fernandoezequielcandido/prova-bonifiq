56bbddf8c541d7ef4c085bd92d5bacfb702b8768
Fernando Ezequiel Candido <87859912+fernandoezequielcandido@users.noreply.github.com>
2023-04-14 10:11:55 -0300

    Update CustomerServiceTests.cs

--- Tests/CustomerServiceTests.cs
+++ Tests/CustomerServiceTests.cs
@@ -66,7 +66,7 @@ namespace ProvaPub.Tests
         }
 
         [Fact]
-        public void CanPurchase_InvalidPurchaseValues()
+        public void CanPurchase_InvalidPurchaseValue()
         {
             //invalid value
             Assert.Throws<System.ArgumentOutOfRangeException>(() => controller4.CanPurchase(10, -20).GetAwaiter().GetResult());
